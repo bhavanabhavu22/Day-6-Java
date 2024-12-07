03a96dfa00988a717eda5a000665dd3944a58713
bhavanabhavu22 <bhavanakallibandla@gmail.com>
2024-12-07 12:36:14 +0530

    Add files via upload

---
+++ MULofnumARR.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class MULofnumARR
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        {
+            int n=sc.nextInt();
+            int arr[]=new int[n];
+            int s=1;
+            for(int i=0;i<n;i++)
+            {
+                arr[i]=sc.nextInt();
+                s*=arr[i];
+
+            }
+            System.out.println(s);
+        }
+    }
+}
+
+
