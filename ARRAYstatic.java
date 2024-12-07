e37d6d58d3ffafd178d0a30893a69a2b3b0f572f
bhavanabhavu22 <bhavanakallibandla@gmail.com>
2024-12-07 14:26:46 +0530

    Add files via upload

---
+++ ARRAYstatic.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class ARRAYstatic
+{
+     public static void main(String args[])
+     {
+        int arr[]={22,11,45,56,77};
+        System.out.println(Arrays.toString(arr));
+        ///accesssing of elements
+        System.out.println(arr[2]);
+        //modification
+         arr[2]=55;
+         arr[3]=455;
+     System.out.println(Arrays.toString(arr));
+   }
+}
